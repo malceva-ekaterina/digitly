2346d6fb4363f06c334204189198c4d100df1e16
Veer.Viktoriya <Veer.Viktoriya@nttek.ru>
2026-06-01 21:31:35 +0500

    test15

--- digitly-frontend/app/layout.tsx
+++ digitly-frontend/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "Цифра - Образовательная платформа",
-  description: "Олимпиады и методические материалы для студентов и школьников",
+  title: "Цифрус - Образовательная платформа",
+  description: "Онлайн олимпиады и методические материалы",
 };
 
 export default function RootLayout({
@@ -23,12 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru"> {/* Изменили en на ru */}
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ru">
+      <body>{children}</body>
     </html>
   );
 }
